5a979ea63c3957243c929b55a70413f516563c9a
Karim <kialimkhanov@edu.hse.ru>
2022-12-07 19:21:34 +0300

    change_channels

--- handlers/change_channel_sampling/change_channel_sampling.py
+++ handlers/change_channel_sampling/change_channel_sampling.py
@@ -1,17 +1,28 @@
-import logging
-
-import aiogram.utils.markdown as md
-from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import FSMContext
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
-from aiogram.types import ParseMode
-from aiogram.dispatcher.filters.state import State, StatesGroup
+from logging.handlers import QueueHandler
 from config import TOKEN as API_TOKEN
-from main import keyboard_start, dp, Digest
+from markups import make_me, array_with_chanels
+from main import keyboard_start, dp, Digest, bot
 
 
-@dp.message_handler(Text(["Изменить выборку каналов"]), state=Digest.confirm_digest)
-async def change_channel_sampling(message: types.Message, state: FSMContext):
-    # TODO
-    # тут типа отправить сообщение выбери каналы
-    await Digest.change_channel_sampling.set()
+@dp.message_handler(Text(["Изменить выборку каналов"]))
+async def change_channel_sampling(callback: QueueHandler):
+    to = callback.from_user.id
+    await bot.send_message(to, 'Выберите ваш канал', reply_markup=make_me('', to))
+
+
+@dp.callback_query_handler(Text(startswith='r'))
+async def check_system(callback: QueueHandler):
+    need_to_send = callback.data
+    to = callback.from_user.id
+    # await bot.delete_message(to, message_id=callback.message.message_id)
+    # await bot.send_message(to,'Выберите ваш канал', reply_markup = make_me(need_to_send, to))
+    menu = make_me(need_to_send, to)
+    array = array_with_chanels(to)
+
+    print(array)
+
+    await callback.message.edit_text('Выберите ваш канал', reply_markup=menu)
+
+
